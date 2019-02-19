d37833d6c4fe13dde1818633bf132b3c6ad34f83
Avi Anand <avi123321123321@gmail.com>
2019-02-19 20:04:02 +0000

    update password to atlas connection

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes     = require("./routes/comments"),
     indexRoutes        = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost/yelp_camp_new");
-mongoose.connect("mongodb+srv://avi:leg@to11@yelpcamp-5mhio.mongodb.net/test?retryWrites=true");
+mongoose.connect("mongodb+srv://avi:aviavi11@yelpcamp-5mhio.mongodb.net/test?retryWrites=true");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
