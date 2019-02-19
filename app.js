1b6a5a02b39348559220f4a5e637ec0c42a31928
Avi Anand <avi123321123321@gmail.com>
2019-02-19 19:59:13 +0000

    add mongodb atlas

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_new");
+//mongoose.connect("mongodb://localhost/yelp_camp_new");
+mongoose.connect("mongodb+srv://avi:leg@to11@yelpcamp-5mhio.mongodb.net/test?retryWrites=true");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
