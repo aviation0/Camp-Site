840dfebf397ab1a77b59e9fff422e5fe00475bd0
Avi Anand <avi123321123321@gmail.com>
2019-02-20 11:43:17 +0000

    environment variable setup complete

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-mongoose.connect(process.env.DATABASEURL);
-//mongoose.connect("mongodb+srv://avi:aviavi11@yelpcamp-5mhio.mongodb.net/yelpcamp");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_new";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
